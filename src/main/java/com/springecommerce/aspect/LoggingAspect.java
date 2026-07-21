d8e9aab83afe6171f7e24c4e747e2b69deeb82d9
Akpevwe11 <essiakpevwepraise@gmail.com>
2026-07-21 16:44:40 +0100

    loggin login

--- src/main/java/com/springecommerce/aspect/LoggingAspect.java
+++ src/main/java/com/springecommerce/aspect/LoggingAspect.java
@@ -1,8 +1,10 @@
 package com.springecommerce.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -12,6 +14,11 @@ import org.springframework.stereotype.Component;
 public class LoggingAspect {
 
     private static final Logger log = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Before("execution(* com.springecommerce.service.AuthenticationService.register(..))")
+    public void logRegister(JoinPoint jp) {
+        log.info("Method callled" + jp.getSignature().getName());
+    }
 
     @Around("execution(* com.springecommerce.controller..*(..))")
     public Object logControllers(ProceedingJoinPoint joinPoint) throws Throwable {
